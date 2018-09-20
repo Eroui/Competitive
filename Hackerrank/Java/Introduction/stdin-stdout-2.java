e0c185ac19cd23c695706b77c201fd7636aa8f0a
Abdelaziz Eroui <abdelaziz.eroui@digimind.com>
2018-09-20 09:43:46 +0100

    Hackerrank Java Introduction Stdin / Stdout 2 Solution

---
+++ Hackerrank/Java/Introduction/stdin-stdout-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = Integer.parseInt(in.nextLine());
+        double d = Double.parseDouble(in.nextLine());
+        String s = in.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
