8d4a04dbec9589136b5f38ca7a4a949659c4b60f
Abdelaziz Eroui <abdelaziz.eroui@digimind.com>
2018-09-20 09:42:55 +0100

    Hackerrank Java Introduction End of File Solution

---
+++ Hackerrank/Java/Introduction/java-end-of-file.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = 0;
+        while(in.hasNext()) {
+            System.out.println((++i)+" "+in.nextLine());
+        }
+    }
+}
