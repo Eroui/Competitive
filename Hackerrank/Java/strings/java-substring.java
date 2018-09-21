8534446b65fba3fb7fcac07de249995fa3766196
Abdelaziz Eroui <kapi.abdel@gmail.com>
2018-09-21 10:22:11 +0100

    Hackerrank Java Substring Solution

---
+++ Hackerrank/Java/strings/java-substring.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        System.out.println(S.substring(in.nextInt(), in.nextInt()));
+    }
+}
